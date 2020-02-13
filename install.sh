67ea0d4791aa78b3e26c3c930ea5725d5410dfd1
Justintime50 <justinpaulhammond@gmail.com>
2020-02-13 11:44:05 -0700

    Bug fixes to install script

--- install.sh
+++ install.sh
@@ -2,24 +2,25 @@
 
 # Pull EasyPost CLI
 echo "Installing EasyPost CLI..."
+EASYPOST_CLI_LOCATION="$HOME"/easypost-cli
 if [[ -d "$HOME"/easypost-cli ]] ; then 
     rm -rf "$HOME"/easypost-cli 
 fi
 git clone https://github.com/Justintime50/easypost-cli.git "$HOME"/ep-cli-temp > /dev/null 2>&1
 
 # Install EasyPost CLI
-mkdir "$HOME"/easypost-cli && mkdir "$HOME"/easypost-cli/bin
+mkdir "$EASYPOST_CLI_LOCATION" && mkdir "$EASYPOST_CLI_LOCATION"/bin
 for DIR in "$HOME"/ep-cli-temp/src/* ; do
-    cp -r "$DIR"/. "$HOME"/easypost-cli/bin
+    cp -r "$DIR"/. "$EASYPOST_CLI_LOCATION"/bin
 done > /dev/null 2>&1
-cp -a "$HOME"/ep-cli-temp/src/easypost-cli.sh "$HOME"/easypost-cli/easypost-cli.sh
-cp -a "$HOME"/ep-cli-temp/src/.env.example "$HOME"/easypost-cli/.env
-cp -a "$HOME"/ep-cli-temp/docs "$HOME"/easypost-cli/docs
+cp -a "$HOME"/ep-cli-temp/src/easypost-cli.sh "$EASYPOST_CLI_LOCATION"/easypost-cli.sh
+cp -a "$HOME"/ep-cli-temp/src/.env.example "$EASYPOST_CLI_LOCATION"/.env
+cp -a "$HOME"/ep-cli-temp/docs "$EASYPOST_CLI_LOCATION"/docs
 rm -rf "$HOME"/ep-cli-temp
 
 # API Key setup
 echo "Enter your EasyPost API key:"
 read -r KEY
 shopt -s dotglob # Ensure we can replace the hidden .env file
-sed -i '' -e "s/EASYPOST_API_KEY=/EASYPOST_API_KEY=$KEY/g" .env
+sed -i '' -e "s/EASYPOST_API_KEY=/EASYPOST_API_KEY=$KEY/g" "$EASYPOST_CLI_LOCATION"/.env
 echo "EasyPost CLI installed!"
