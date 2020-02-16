5d9de15d6bd5be0b566f1b150f70c001c622427b
Justintime50 <justinpaulhammond@gmail.com>
2020-02-15 22:16:18 -0700

    Updated install script

--- install.sh
+++ install.sh
@@ -2,12 +2,15 @@
 
 echo "Installing EasyPost CLI..."
 EASYPOST_CLI_LOCATION="/usr/local/bin"
+
+# Remove the executable if it exists already
 if [[ -f "$EASYPOST_CLI_LOCATION"/ep ]] ; then
     rm -f "$EASYPOST_CLI_LOCATION"/ep
 fi
 
-# Download the EasyPost CLI
-curl -s https://raw.githubusercontent.com/justintime50/easypost-cli/master/src/ep $EASYPOST_CLI_LOCATION
+# Install the EasyPost CLI
+curl -s https://raw.githubusercontent.com/justintime50/easypost-cli/master/src/ep > "$EASYPOST_CLI_LOCATION"/ep
+chmod +x "$EASYPOST_CLI_LOCATION"/ep
 
 # API Key setup
 echo "Enter your EasyPost API key:"
