13fc87cc0e1473cfceecd24a76b26c9fff7635f6
Justintime50 <justinpaulhammond@gmail.com>
2020-02-19 13:51:42 -0700

    Added reports endpoint

---
+++ src/reports/create-reports.sh
@@ -0,0 +1,60 @@
+
+function create-payment-log-report {
+    # ep create-payment-log-report: Create a payment log report
+    # Prompt user for input
+    echo "Enter a start date (eg: 2016-10-01):"
+    read -r STARTDATE
+    echo "Enter an end date (eg: 2016-10-31):"
+    read -r ENDDATE
+
+    # Build curl request
+    curl -s -X GET "$EASYPOST_API_URL"/reports/payment_log/"$REPORT" \
+    -u "$EASYPOST_CLI_API_KEY": \
+    -d "{'start_date':$STARTDATE,'end_date':$ENDDATE}" \
+    | json_pp
+}
+
+function create-refund-report {
+    # ep create-refund-report: Create a payment log report
+    # Prompt user for input
+    echo "Enter a start date (eg: 2016-10-01):"
+    read -r STARTDATE
+    echo "Enter an end date (eg: 2016-10-31):"
+    read -r ENDDATE
+
+    # Build curl request
+    curl -s -X GET "$EASYPOST_API_URL"/reports/refund/"$REPORT" \
+    -u "$EASYPOST_CLI_API_KEY": \
+    -d "{'start_date':$STARTDATE,'end_date':$ENDDATE}" \
+    | json_pp
+}
+
+function create-shipment-report {
+    # ep create-shipment-report: Create a payment log report
+    # Prompt user for input
+    echo "Enter a start date (eg: 2016-10-01):"
+    read -r STARTDATE
+    echo "Enter an end date (eg: 2016-10-31):"
+    read -r ENDDATE
+
+    # Build curl request
+    curl -s -X GET "$EASYPOST_API_URL"/reports/shipment/"$REPORT" \
+    -u "$EASYPOST_CLI_API_KEY": \
+    -d "{'start_date':$STARTDATE,'end_date':$ENDDATE}" \
+    | json_pp
+}
+
+function create-tracker-report {
+    # ep create-tracker-report: Create a payment log report
+    # Prompt user for input
+    echo "Enter a start date (eg: 2016-10-01):"
+    read -r STARTDATE
+    echo "Enter an end date (eg: 2016-10-31):"
+    read -r ENDDATE
+
+    # Build curl request
+    curl -s -X GET "$EASYPOST_API_URL"/reports/tracker/"$REPORT" \
+    -u "$EASYPOST_CLI_API_KEY": \
+    -d "{'start_date':$STARTDATE,'end_date':$ENDDATE}" \
+    | json_pp 
+}
---
+++ src/reports/retrieve-report.sh
@@ -0,0 +1,48 @@
+
+function retrieve-payment-log-report {
+    # ep retrieve-payment-log-report: Retrieve a single payment log report
+    # Prompt user for input
+    echo "Enter report ID:"
+    read -r REPORT
+
+    # Build curl request
+    curl -s -X GET "$EASYPOST_API_URL"/report/payment_log/"$REPORT" \
+    -u "$EASYPOST_CLI_API_KEY": \
+    | json_pp 
+}
+
+function retrieve-refund-report {
+    # ep retrieve-refund-report: Retrieve a single payment log report
+    # Prompt user for input
+    echo "Enter report ID:"
+    read -r REPORT
+
+    # Build curl request
+    curl -s -X GET "$EASYPOST_API_URL"/report/refund/"$REPORT" \
+    -u "$EASYPOST_CLI_API_KEY": \
+    | json_pp 
+}
+
+function retrieve-shipment-report {
+    # ep retrieve-shipment-report: Retrieve a single payment log report
+    # Prompt user for input
+    echo "Enter report ID:"
+    read -r REPORT
+
+    # Build curl request
+    curl -s -X GET "$EASYPOST_API_URL"/report/shipment/"$REPORT" \
+    -u "$EASYPOST_CLI_API_KEY": \
+    | json_pp 
+}
+
+function retrieve-tracker-report {
+    # ep retrieve-tracker-report: Retrieve a single payment log report
+    # Prompt user for input
+    echo "Enter report ID:"
+    read -r REPORT
+
+    # Build curl request
+    curl -s -X GET "$EASYPOST_API_URL"/report/tracker/"$REPORT" \
+    -u "$EASYPOST_CLI_API_KEY": \
+    | json_pp 
+}
