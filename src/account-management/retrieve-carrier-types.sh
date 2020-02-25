d0853bcc275b560b25d078a389558b008ca7d1b0
Justintime50 <justinpaulhammond@gmail.com>
2020-02-25 12:09:41 -0700

    Added carrier types

---
+++ src/account-management/retrieve-carrier-types.sh
@@ -0,0 +1,8 @@
+
+function retrieve-carrier-types {
+    # ep retrieve-carrier-types: Retrieve the list of all carrier types available to the account of the given API key.
+    # Build curl request
+    curl -s -X GET "$EASYPOST_API_URL"/carrier_types \
+    -u "$EASYPOST_CLI_API_KEY": \
+    | json_pp
+}
