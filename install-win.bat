5641598816d2312e1f67fc65956b2230d5ca2086
Justintime50 <justinpaulhammond@gmail.com>
2020-03-07 21:05:40 -0700

    Bug fixes to Windows build and docs

--- install-win.bat
+++ install-win.bat
@@ -8,9 +8,10 @@ mkdir %LOCATION%
 cd %LOCATION%
 curl -LJOs https://raw.githubusercontent.com/justintime50/easypost-cli/master/executables/windows/easypost-cli-%VERSION%.tgz
 tar -xf easypost-cli-%VERSION%.tgz
-:: PowerShell Expand-Archive "C:\easypost-cli\easypost-cli-%VERSION%.zip" "C:\easypost-cli"
 
 echo Attempting to install jq...
 choco install jq
+
+del /f easypost-cli-%VERSION%.tgz
 
 echo EasyPost CLI installed! Make sure to setup your API key.
