cdaa6df6eeae17a41e831f8b803760e0111b1616
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-05-01 13:12:35 -0600

    Fixing lint

--- dist/unix_ep.sh
+++ dist/unix_ep.sh
@@ -6,7 +6,7 @@
 ################################################
 
 # Init Functions
-# shellcheck disable=SC1090
+# shellcheck disable=SC1090,SC1091
 
 main() {
     # Run this main function anytime the CLI is used
